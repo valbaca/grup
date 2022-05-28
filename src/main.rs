0c9790f2f85d471e8e9d31fd010b5a858c588411
Valentin Baca <valbaca@gmail.com>
2022-05-28 16:28:09 -0700

    end of 12.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// it's convention to bring in the parent module in scope, rather than the fn
+// this makes it clearer where fns are coming from
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    // The type on arg tells Rust what kind of collect() to run
+    let args: Vec<String> = env::args().collect();
+
+    // println!("{:?}", args);
+    //> ["target/debug/grup", "needle", "haystack"]
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
