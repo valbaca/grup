d7bf61454a0b2e8258dc3d7be8a0ac97ececd3fb
Valentin Baca <valbaca@gmail.com>
2022-05-29 11:23:10 -0700

    Parse and handle too few args

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 // this makes it clearer where fns are coming from
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     // The type on arg tells Rust what kind of collect() to run
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -23,9 +27,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
