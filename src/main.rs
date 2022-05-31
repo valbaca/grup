223d261b52e0931333aeeb1d0a858f7e8565574f
Valentin Baca <valbaca@gmail.com>
2022-05-30 23:43:02 -0700

    using iterators

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ use std::env;
 use std::process;
 fn main() {
     // The type on arg tells Rust what kind of collect() to run
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
